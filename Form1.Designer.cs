455b11ce5ca89b1fa9720d1fda53817f0141aad2
ElEkTrOnIc <75856311+Fabiors-81@users.noreply.github.com>
2022-08-28 23:13:57 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace MorraCinese
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lbl_MorraCinese1 = new System.Windows.Forms.Label();
+            this.lbl_SaccoCartaForbice1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_zero2 = new System.Windows.Forms.Label();
+            this.lbl_zero1 = new System.Windows.Forms.Label();
+            this.btn_Precedente = new System.Windows.Forms.Button();
+            this.btn_Seleziona = new System.Windows.Forms.Button();
+            this.btn_Successivo = new System.Windows.Forms.Button();
+            this.label_Sconosciuto = new System.Windows.Forms.Label();
+            this.lbl_Sconosciuto = new System.Windows.Forms.Label();
+            this.label_VS = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbl_MorraCinese2 = new System.Windows.Forms.Label();
+            this.lbl_SaccoCartaForbice2 = new System.Windows.Forms.Label();
+            this.lbl_Modalità_Gioco = new System.Windows.Forms.Label();
+            this.btn_2Giocatore = new System.Windows.Forms.Button();
+            this.btn_1Giocatore = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::MorraCinese.Properties.Resources.sconosciuto;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(397, 284);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::MorraCinese.Properties.Resources.sconosciuto;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(824, 271);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(257, 251);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lbl_MorraCinese1
+            // 
+            this.lbl_MorraCinese1.AutoSize = true;
+            this.lbl_MorraCinese1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MorraCinese1.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.lbl_MorraCinese1.Location = new System.Drawing.Point(347, 0);
+            this.lbl_MorraCinese1.Name = "lbl_MorraCinese1";
+            this.lbl_MorraCinese1.Size = new System.Drawing.Size(574, 98);
+            this.lbl_MorraCinese1.TabIndex = 2;
+            this.lbl_MorraCinese1.Text = "Morra Cinese";
+            // 
+            // lbl_SaccoCartaForbice1
+            // 
+            this.lbl_SaccoCartaForbice1.AutoSize = true;
+            this.lbl_SaccoCartaForbice1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_SaccoCartaForbice1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lbl_SaccoCartaForbice1.Location = new System.Drawing.Point(353, 87);
+            this.lbl_SaccoCartaForbice1.Name = "lbl_SaccoCartaForbice1";
+            this.lbl_SaccoCartaForbice1.Size = new System.Drawing.Size(537, 63);
+            this.lbl_SaccoCartaForbice1.TabIndex = 3;
+            this.lbl_SaccoCartaForbice1.Text = "Sasso Carta Forbice";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(573, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(256, 54);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Punteggio:";
+            // 
+            // lbl_zero2
+            // 
+            this.lbl_zero2.AutoSize = true;
+            this.lbl_zero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_zero2.ForeColor = System.Drawing.Color.Black;
+            this.lbl_zero2.Location = new System.Drawing.Point(907, 205);
+            this.lbl_zero2.Name = "lbl_zero2";
+            this.lbl_zero2.Size = new System.Drawing.Size(70, 76);
+            this.lbl_zero2.TabIndex = 5;
+            this.lbl_zero2.Text = "0";
+            // 
+            // lbl_zero1
+            // 
+            this.lbl_zero1.AutoSize = true;
+            this.lbl_zero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_zero1.ForeColor = System.Drawing.Color.Black;
+            this.lbl_zero1.Location = new System.Drawing.Point(450, 205);
+            this.lbl_zero1.Name = "lbl_zero1";
+            this.lbl_zero1.Size = new System.Drawing.Size(70, 76);
+            this.lbl_zero1.TabIndex = 6;
+            this.lbl_zero1.Text = "0";
+            // 
+            // btn_Precedente
+            // 
+            this.btn_Precedente.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btn_Precedente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Precedente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Precedente.ForeColor = System.Drawing.Color.Black;
+            this.btn_Precedente.Location = new System.Drawing.Point(111, 284);
+            this.btn_Precedente.Name = "btn_Precedente";
+            this.btn_Precedente.Size = new System.Drawing.Size(225, 76);
+            this.btn_Precedente.TabIndex = 7;
+            this.btn_Precedente.Text = "Precedente";
+            this.btn_Precedente.UseVisualStyleBackColor = false;
+            // 
+            // btn_Seleziona
+            // 
+            this.btn_Seleziona.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btn_Seleziona.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Seleziona.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Seleziona.ForeColor = System.Drawing.Color.Black;
+            this.btn_Seleziona.Location = new System.Drawing.Point(111, 459);
+            this.btn_Seleziona.Name = "btn_Seleziona";
+            this.btn_Seleziona.Size = new System.Drawing.Size(225, 76);
+            this.btn_Seleziona.TabIndex = 8;
+            this.btn_Seleziona.Text = "Seleziona";
+            this.btn_Seleziona.UseVisualStyleBackColor = false;
+            // 
+            // btn_Successivo
+            // 
+            this.btn_Successivo.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btn_Successivo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Successivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Successivo.ForeColor = System.Drawing.Color.Black;
+            this.btn_Successivo.Location = new System.Drawing.Point(111, 366);
+            this.btn_Successivo.Name = "btn_Successivo";
+            this.btn_Successivo.Size = new System.Drawing.Size(225, 76);
+            this.btn_Successivo.TabIndex = 9;
+            this.btn_Successivo.Text = "Successivo";
+            this.btn_Successivo.UseVisualStyleBackColor = false;
+            // 
+            // label_Sconosciuto
+            // 
+            this.label_Sconosciuto.AutoSize = true;
+            this.label_Sconosciuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Sconosciuto.ForeColor = System.Drawing.Color.Black;
+            this.label_Sconosciuto.Location = new System.Drawing.Point(390, 554);
+            this.label_Sconosciuto.Name = "label_Sconosciuto";
+            this.label_Sconosciuto.Size = new System.Drawing.Size(214, 39);
+            this.label_Sconosciuto.TabIndex = 11;
+            this.label_Sconosciuto.Text = "Sconosciuto";
+            // 
+            // lbl_Sconosciuto
+            // 
+            this.lbl_Sconosciuto.AutoSize = true;
+            this.lbl_Sconosciuto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Sconosciuto.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Sconosciuto.Location = new System.Drawing.Point(817, 554);
+            this.lbl_Sconosciuto.Name = "lbl_Sconosciuto";
+            this.lbl_Sconosciuto.Size = new System.Drawing.Size(214, 39);
+            this.lbl_Sconosciuto.TabIndex = 12;
+            this.lbl_Sconosciuto.Text = "Sconosciuto";
+            // 
+            // label_VS
+            // 
+            this.label_VS.AutoSize = true;
+            this.label_VS.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_VS.ForeColor = System.Drawing.Color.Firebrick;
+            this.label_VS.Location = new System.Drawing.Point(648, 384);
+            this.label_VS.Name = "label_VS";
+            this.label_VS.Size = new System.Drawing.Size(156, 83);
+            this.label_VS.TabIndex = 13;
+            this.label_VS.Text = "VS.";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_MorraCinese1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label_VS);
+            this.panel1.Controls.Add(this.lbl_SaccoCartaForbice1);
+            this.panel1.Controls.Add(this.lbl_Sconosciuto);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label_Sconosciuto);
+            this.panel1.Controls.Add(this.lbl_zero2);
+            this.panel1.Controls.Add(this.btn_Successivo);
+            this.panel1.Controls.Add(this.lbl_zero1);
+            this.panel1.Controls.Add(this.btn_Seleziona);
+            this.panel1.Controls.Add(this.btn_Precedente);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1155, 619);
+            this.panel1.TabIndex = 14;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lbl_MorraCinese2);
+            this.panel2.Controls.Add(this.lbl_SaccoCartaForbice2);
+            this.panel2.Controls.Add(this.lbl_Modalità_Gioco);
+            this.panel2.Controls.Add(this.btn_2Giocatore);
+            this.panel2.Controls.Add(this.btn_1Giocatore);
+            this.panel2.Location = new System.Drawing.Point(12, 637);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1155, 619);
+            this.panel2.TabIndex = 15;
+            // 
+            // lbl_MorraCinese2
+            // 
+            this.lbl_MorraCinese2.AutoSize = true;
+            this.lbl_MorraCinese2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MorraCinese2.ForeColor = System.Drawing.Color.DarkSeaGreen;
+            this.lbl_MorraCinese2.Location = new System.Drawing.Point(296, 27);
+            this.lbl_MorraCinese2.Name = "lbl_MorraCinese2";
+            this.lbl_MorraCinese2.Size = new System.Drawing.Size(574, 98);
+            this.lbl_MorraCinese2.TabIndex = 2;
+            this.lbl_MorraCinese2.Text = "Morra Cinese";
+            // 
+            // lbl_SaccoCartaForbice2
+            // 
+            this.lbl_SaccoCartaForbice2.AutoSize = true;
+            this.lbl_SaccoCartaForbice2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_SaccoCartaForbice2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lbl_SaccoCartaForbice2.Location = new System.Drawing.Point(302, 114);
+            this.lbl_SaccoCartaForbice2.Name = "lbl_SaccoCartaForbice2";
+            this.lbl_SaccoCartaForbice2.Size = new System.Drawing.Size(537, 63);
+            this.lbl_SaccoCartaForbice2.TabIndex = 3;
+            this.lbl_SaccoCartaForbice2.Text = "Sasso Carta Forbice";
+            // 
+            // lbl_Modalità_Gioco
+            // 
+            this.lbl_Modalità_Gioco.AutoSize = true;
+            this.lbl_Modalità_Gioco.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Modalità_Gioco.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Modalità_Gioco.Location = new System.Drawing.Point(379, 210);
+            this.lbl_Modalità_Gioco.Name = "lbl_Modalità_Gioco";
+            this.lbl_Modalità_Gioco.Size = new System.Drawing.Size(399, 54);
+            this.lbl_Modalità_Gioco.TabIndex = 4;
+            this.lbl_Modalità_Gioco.Text = "Modalità di Gioco";
+            // 
+            // btn_2Giocatore
+            // 
+            this.btn_2Giocatore.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btn_2Giocatore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2Giocatore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2Giocatore.ForeColor = System.Drawing.Color.Black;
+            this.btn_2Giocatore.Location = new System.Drawing.Point(824, 284);
+            this.btn_2Giocatore.Name = "btn_2Giocatore";
+            this.btn_2Giocatore.Size = new System.Drawing.Size(225, 76);
+            this.btn_2Giocatore.TabIndex = 9;
+            this.btn_2Giocatore.Text = "2 Giocatore";
+            this.btn_2Giocatore.UseVisualStyleBackColor = false;
+            // 
+            // btn_1Giocatore
+            // 
+            this.btn_1Giocatore.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btn_1Giocatore.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1Giocatore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1Giocatore.ForeColor = System.Drawing.Color.Black;
+            this.btn_1Giocatore.Location = new System.Drawing.Point(111, 284);
+            this.btn_1Giocatore.Name = "btn_1Giocatore";
+            this.btn_1Giocatore.Size = new System.Drawing.Size(225, 76);
+            this.btn_1Giocatore.TabIndex = 7;
+            this.btn_1Giocatore.Text = "1 Giocatore";
+            this.btn_1Giocatore.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1274, 1391);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lbl_MorraCinese1;
+        private System.Windows.Forms.Label lbl_SaccoCartaForbice1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_zero2;
+        private System.Windows.Forms.Label lbl_zero1;
+        private System.Windows.Forms.Button btn_Precedente;
+        private System.Windows.Forms.Button btn_Seleziona;
+        private System.Windows.Forms.Button btn_Successivo;
+        private System.Windows.Forms.Label label_Sconosciuto;
+        private System.Windows.Forms.Label lbl_Sconosciuto;
+        private System.Windows.Forms.Label label_VS;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbl_MorraCinese2;
+        private System.Windows.Forms.Label lbl_SaccoCartaForbice2;
+        private System.Windows.Forms.Label lbl_Modalità_Gioco;
+        private System.Windows.Forms.Button btn_2Giocatore;
+        private System.Windows.Forms.Button btn_1Giocatore;
+    }
+}
+
